897eff41826e7d767723ebc77fc7695f811ed848
Vadim Volkov <gnmisto@gmail.com>
2016-12-01 20:11:56 +0200

    Header code style fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React, { PropTypes, Component } from 'react';
 
 export default class Header extends Component {
-		render() {
-				const { name } = this.props
-				return	<div>
-							<p>Hello, {name}!</p>
-						</div>
-	}
+  render() {
+    const { name } = this.props;
+    return (
+      <div>
+        <p>{name}</p>
+      </div>
+    );
+  }
 }
+
 Header.propTypes = {
-		name: PropTypes.string.isRequired
-}
+  name: PropTypes.string.isRequired
+};
